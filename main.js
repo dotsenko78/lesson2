27f7901f3783c9a54a869deb400410b92005f3eb
dotsenko78 <92103187+dotsenko78@users.noreply.github.com>
2022-01-14 19:51:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+// let a = 0
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = 10n
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = true
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = 'foo'
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = 'Symbol'
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = Math
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = null
+
+// console.log(a)
+// console.log(typeof(a))
+
+// a = alert
+
+// console.log(a)
+// console.log(typeof(a))
+
+
+
+
+// let a = prompt('Введите число', 3)
+// console.log(a)
+// console.log(typeof(a))
+
+// let a = Number (prompt('первое число', '1'))
+// let b = Number (prompt('второе число', '2'))
+// c=a+b
+// alert(c)
+
+// или
+
+// let a = +prompt('первое число', '1')
+// let b = +prompt('второе число', '2')
+// c=a+b
+// alert(c)
+
+
+// let userName = 'Ilya'
+// alert(`hello ${1}`)
+// alert(`hello ${'userName'}`)
+// alert(`hello ${userName}`)
+// alert(`hello ${1+3+5}`)
+
+
+// let a = Boolean(prompt('введите число'))
+// console.log (a)
+// console.log (typeof(a))
+
+
+const text = document.querySelector('.title')
+
+text.style.fontSize = '20px'
+text.style.color = 'pink'
+
+function clickSize() {
+    text.style.fontSize = '50px'
+    text.style.color = 'blue'
+}
+
